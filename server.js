a480af721d8beb0c8bacd9cb4550b080f82bcacf
Joblyn <oaikhenahjob@gmail.com>
2022-02-26 00:28:58 +0100

    updated pg database

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const db = knex({
   // Enter your own database information here based on what you created
   client: "pg",
   connection: {
-    host: "postgresql-acute-94816",
-    user: "joblyn",
-    password: "",
-    database: "smart-brain",
+    host: process.env.DATABASE_URL,
+    ssl:true
   },
 });
 
